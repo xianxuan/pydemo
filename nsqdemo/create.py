397b3f1a25bd5ca6366048731c59a3dff61c1b94
ykx <2418606562@qq.com>
2018-04-17 17:35:50 +0800

    nsq

---
+++ nsqdemo/create.py
@@ -0,0 +1,15 @@
+import nsq
+import tornado.ioloop
+import time
+
+def pub_message():
+    msg = "nihao"
+    #转为bytes数组
+    writer.pub('test', msg.encode(encoding="utf-8"), finish_pub)
+
+def finish_pub(conn, data):
+    print(data)
+
+writer = nsq.Writer(['127.0.0.1:4150'])
+tornado.ioloop.PeriodicCallback(pub_message, 1000).start()
+nsq.run()
